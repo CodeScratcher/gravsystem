e40e4c8b5c21379cdde2eddeda995af7f6fdea7c
afisher <bb8andr2d2forlife@gmail.com>
2024-11-29 21:22:13 -0500

    Added pause

--- core/src/main/java/com/skiscratcher/GravitySim/Main.java
+++ core/src/main/java/com/skiscratcher/GravitySim/Main.java
@@ -3,9 +3,8 @@ package com.skiscratcher.GravitySim;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
@@ -23,11 +22,17 @@ public class Main extends ApplicationAdapter {
     private final double scale = 1e7;
     private final double timescale = 10000;
     private List<Object> objects;
+    private double time = 0;
+    private boolean paused = false;
+
+    private BitmapFont fnt;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         sr = new ShapeRenderer();
+
+        fnt = new BitmapFont();
 
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
@@ -38,42 +43,86 @@ public class Main extends ApplicationAdapter {
         objects = new ArrayList<>();
         // objects.add(new Object(new Vector2D(0, 0), new Vector2D(0, 0), 1.988400e30));
         // objects.add(new Object(new Vector2D(152099968880.0, 0), new Vector2D(0, 29290), 5.9722e24));
-        objects.add(new Object(new Vector2D(0, 0), new Vector2D(0, 0), 9.45996e24));
-        objects.add(new Object(new Vector2D(416490000, 0), new Vector2D(0, 1243.49862203), 1.06e20));
+        objects.add(new Object(new Vector2D(0, 0), new Vector2D(0, 0), 9.45996e24, new Color(0.3f, 0.7f, 1.0f, 1.0f), 5.0));
+        objects.add(new Object(new Vector2D(416490000, 0), new Vector2D(0, 1243.49862203), 1.06e22, new Color(0.6f, 0.6f, 0.6f, 1.0f), 1.0));
+        objects.add(new Object(new Vector2D(56541880, 0), new Vector2D(0, 3400), 450000, new Color(1, 1, 1, 1), 0.5));
+        objects.add(new Object(new Vector2D(416490000 - 400000, 0), new Vector2D(0, -200), 450000, new Color(1, 1, 1, 1), 0.5));
+        objects.add(new Object(new Vector2D(416490000 - 400000, 0), new Vector2D(0, -400), 450000, new Color(1, 1, 1, 1), 0.5));
     }
 
     @Override
     public void render() {
-       if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            cam.zoom += 0.02f;
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+             cam.zoom += 0.02f;
+             ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
+            cam.zoom -= 0.02f;
             ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-       }
-       if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
-           cam.zoom -= 0.02f;
-           ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-       }
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            paused = !paused;
+            ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        }
 
 
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
 
-       batch.begin();
+        Camera cam2 = new OrthographicCamera(30, 30 * (h / w));
+        cam2.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
 
-       batch.end();
 
-       cam.position.x = (float)(objects.get(objFollowed).getPosition().getX() / scale);
-       cam.position.y = (float)(objects.get(objFollowed).getPosition().getY() / scale);
-       cam.update();
+        cam.position.x = (float)(objects.get(objFollowed).getPosition().getX() / scale);
+        cam.position.y = (float)(objects.get(objFollowed).getPosition().getY() / scale);
+        cam.update();
 
-       sr.setProjectionMatrix(cam.combined);
 
-       sr.begin(ShapeType.Filled);
-       sr.setColor(1, 1, 0, 1);
+        sr.setProjectionMatrix(cam2.combined);
+        sr.begin(ShapeType.Filled);
 
-       for (Object obj : objects) {
-           obj.update(objects, Gdx.graphics.getDeltaTime() * timescale);
-           sr.circle((float)(obj.getPosition().getX() / scale), (float)(obj.getPosition().getY() / scale), 5);
-       }
+        sr.setColor(new Color(1, 1, 1, 1));
+        // sr.rect(-15, -15 * (h / w), 10, 2.5f);
 
-       sr.end();
+        sr.setProjectionMatrix(cam.combined);
+
+        if (!paused) {
+            for (int i = 0; i < 10000; i++) {
+                for (Object obj : objects) {
+                    sr.setColor(obj.getColor());
+
+                    obj.update(objects, Gdx.graphics.getDeltaTime() * timescale / 10000);
+
+                    sr.circle((float)(obj.getPosition().getX() / scale), (float)(obj.getPosition().getY() / scale), (float)obj.getRadius());
+
+                }
+            }
+
+            time += timescale * Gdx.graphics.getDeltaTime();
+
+            int secs  = (int)time % 60;
+            double min = time / 60;
+            int leftoverMin = (int)min % 60;
+            double hr = min / 60;
+            int leftoverHr = (int)hr % 24;
+            int day = (int)hr / 24;
+            Gdx.graphics.setTitle(day + "days, " + leftoverHr + "hours, " + leftoverMin + "minutes, " + secs + "seconds.");
+
+        }
+        else {
+            for (Object obj : objects) {
+                sr.setColor(obj.getColor());
+                sr.circle((float)(obj.getPosition().getX() / scale), (float)(obj.getPosition().getY() / scale), (float)obj.getRadius());
+            }
+        }
+
+
+        sr.end();
+
+
+        batch.begin();
+
+        batch.end();
     }
 
     @Override
--- core/src/main/java/com/skiscratcher/GravitySim/Object.java
+++ core/src/main/java/com/skiscratcher/GravitySim/Object.java
@@ -6,6 +6,8 @@ package com.skiscratcher.GravitySim;
 
 import java.text.NumberFormat;
 import java.util.List;
+
+import com.badlogic.gdx.graphics.Color;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
@@ -26,13 +28,18 @@ public class Object {
     private Vector2D acceleration = new Vector2D(0, 0);
     private Vector2D force = new Vector2D(0, 0);
 
+    private Color color;
+    private double radius;
+
     private final double mass;
     private final double G = 6.67430e-11;
 
-    public Object(Vector2D position, Vector2D velocity, double mass) {
+    public Object(Vector2D position, Vector2D velocity, double mass, Color color, double radius) {
         this.position = position;
         this.velocity = velocity;
         this.mass = mass;
+        this.color = color;
+        this.radius = radius;
     }
 
     /**
@@ -41,14 +48,13 @@ public class Object {
      * @param delta Time since last frame in seconds per frame
      */
     public void update(List<Object> objects, double delta) {
-        Vector2D midVelocity = velocity.add(acceleration.scalarMultiply(delta / 2));
-        position = position.add(midVelocity.scalarMultiply(delta));
+        velocity = velocity.add(acceleration.scalarMultiply(delta / 2));
+        position = position.add(velocity.scalarMultiply(delta));
 
         applyGravity(objects);
         acceleration = force.scalarMultiply(1 / mass);
-        System.out.println(acceleration.toString(NumberFormat.getNumberInstance()));
 
-        velocity = midVelocity.add(acceleration.scalarMultiply(delta / 2));
+        velocity = velocity.add(acceleration.scalarMultiply(delta / 2));
     }
     /**
      * Applies gravity
@@ -57,7 +63,7 @@ public class Object {
     public void applyGravity(List<Object> objects) {
         force = new Vector2D(0.0, 0.0);
         for (Object object : objects) {
-            if (object != this) {
+            if (object != this && !object.getPosition().equals(position)) {
                 double pull = G * ((mass * object.mass) / (Math.pow(position.distance(object.position), 2)));
                 // System.out.println(pull);
                 force = force.add(object.position.subtract(position).normalize().scalarMultiply(pull));
